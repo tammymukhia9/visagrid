7210bce2af05523f9405e25062c74fdfe59e452d
Tamanna <tmtamanna84@gmail.com>
2019-04-25 23:46:36 -0400

    image bug

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CommentTest < ActiveSupport::TestCase
 
 	 def setup
-		 @comment = Comment.create(body: "This is a comment", member_id: 1, visa_info_id: 1)
+	 	@comment = Comment.create(body: "This is a comment", member_id: 1, visa_info_id: 1)
  	 end
 	 test "comment must be valid" do
 	 	assert @comment.valid?
